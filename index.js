8689e807582a715fd8142e8a25195821eabb3ca2
martin-coding <martinkoehn.coding@gmail.com>
2023-08-30 19:38:17 +0200

    Static view
    Created the pages with static files (page layout)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
 
 /* Reading global variables from config file */
 dotenv.config();
@@ -7,8 +8,20 @@ const PORT = process.env.PORT;
 
 app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello weatherlog!");
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.get("/", function(request, response) {
+    response.render("index");
+});
+
+app.get("/dashboard", function(request, response) {
+    response.render("dashboard");
+});
+
+app.get("/stations/:id", function(request, response) {
+    response.render("stations");
 });
 
 app.listen(PORT, function() {
